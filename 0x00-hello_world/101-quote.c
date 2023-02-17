eee4f2af504f42f8f17773c5d83c935b318af05a
Witzyjim <witzyjim@gmail.com>
2023-02-16 18:53:37 -0800

    optional task 08

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <errno.h>
+/**
+ * main - This program outputs to standard error
+ * Return: 1
+ */
+int main(void)
+{
+	fprint("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
