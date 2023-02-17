c3e14ad47008d5cd83311c186ff87d186d90be36
Witzyjim <witzyjim@gmail.com>
2023-02-16 17:06:50 -0800

    task 04

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This programe uses the function put.c to print a some input
+ * Return: 0 (zero value is returned upon successful exceution of the program)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
