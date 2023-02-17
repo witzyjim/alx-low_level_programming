b6ad8d4322d8ff309aace07caa8845886d1a09f0
Witzyjim <witzyjim@gmail.com>
2023-02-16 19:07:12 -0800

    optional task 08

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <errno.h>
 /**
  * main - This program outputs to standard error
  * Return: 1
  */
 int main(void)
 {
-	fprint("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char _char = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	putchar(_char);
 	return (1);
 }
