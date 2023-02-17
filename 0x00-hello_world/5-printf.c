7879fff004e9a02c247740549d62e325be11e7af
Witzyjim <witzyjim@gmail.com>
2023-02-16 17:13:36 -0800

    task 05

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This program uses the printf function to output a sentence
+ * Return: 0 (It returns Zero upon success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
